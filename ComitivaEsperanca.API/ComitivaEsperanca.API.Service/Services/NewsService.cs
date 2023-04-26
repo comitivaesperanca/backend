6a339a2943a7076a254ff57abea3dfef59ab1367
Eduardo Godoy <eduardo.godoy@ufms.br>
2023-04-26 01:25:19 -0300

    Adicionado AutoMapper

---
+++ ComitivaEsperanca.API/ComitivaEsperanca.API.Service/Services/NewsService.cs
@@ -0,0 +1,11 @@
+﻿using ComitivaEsperanca.API.Domain.Interfaces.UnitOfWork;
+
+namespace ComitivaEsperanca.API.Service.Services
+{
+    public class NewsService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+    }
+}
