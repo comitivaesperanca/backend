f4e1a7df9eb224f61d664f8b0d89c956c710ffef
Eduardo Fuso <eduardo.fuso@sesims.com.br>
2023-04-26 21:16:03 -0400

    Implementando Service para entidade Noticia

--- ComitivaEsperanca.API/ComitivaEsperanca.API.Service/Services/NewsService.cs
+++ ComitivaEsperanca.API/ComitivaEsperanca.API.Service/Services/NewsService.cs
@@ -1,7 +1,10 @@
 ﻿using AutoMapper;
 using ComitivaEsperanca.API.Data.Context;
 using ComitivaEsperanca.API.Data.UnitOfWork;
+using ComitivaEsperanca.API.Domain.DTOs;
+using ComitivaEsperanca.API.Domain.Entities;
 using ComitivaEsperanca.API.Domain.Interfaces.UnitOfWork;
+using Microsoft.AspNetCore.Http;
 
 namespace ComitivaEsperanca.API.Service.Services
 {
@@ -14,6 +17,43 @@ namespace ComitivaEsperanca.API.Service.Services
         {
             _unitOfWork = new UnitOfWork(context);
             _mapper = mapper;
+        }
+
+        public ResponseDTO<News> CreateNews(NewsDTO newsDTO)
+        {
+            #region [Validations]
+
+            #endregion
+
+            try
+            {
+                var news = _mapper.Map<News>(newsDTO);
+                _unitOfWork.NewsRepository.Add(news);
+             
+                if (_unitOfWork.Commit() > 0)
+                    return new ResponseDTO<News>(StatusCodes.Status201Created, news);
+
+                return new ResponseDTO<News>(StatusCodes.Status400BadRequest);
+            }
+            catch(Exception ex)
+            {
+                return new ResponseDTO<News>(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        public ResponseDTO<News> GetNewsById(Guid id)
+        {
+            try
+            {
+                var news = _unitOfWork.NewsRepository.Get(x => x.Id == id);
+                if (news == null)
+                    return new ResponseDTO<News>(StatusCodes.Status404NotFound);
+                return new ResponseDTO<News>(StatusCodes.Status200OK, news);
+            }
+            catch (Exception ex)
+            {
+                return new ResponseDTO<News>(StatusCodes.Status500InternalServerError, ex.Message);
+            }
         }
     }
 }
