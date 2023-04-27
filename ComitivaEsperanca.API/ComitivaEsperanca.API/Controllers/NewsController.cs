95d5c6e798ff1a351778fbdb723567c3cceec24d
Eduardo Fuso <eduardo.fuso@sesims.com.br>
2023-04-26 21:13:05 -0400

    Implementando controller de Noticias

--- ComitivaEsperanca.API/ComitivaEsperanca.API/Controllers/NewsController.cs
+++ ComitivaEsperanca.API/ComitivaEsperanca.API/Controllers/NewsController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ComitivaEsperanca.API.Domain.DTOs;
+using ComitivaEsperanca.API.Domain.Entities;
+using ComitivaEsperanca.API.Service.Services;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ComitivaEsperanca.API.Controllers
 {
@@ -6,6 +9,31 @@ namespace ComitivaEsperanca.API.Controllers
     [ApiController]
     public class NewsController : Controller
     {
+        private readonly NewsService _newsService;
+
+        public NewsController(NewsService newsService)
+        {
+            _newsService = newsService;
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created, Type = typeof(ResponseDTO<NewsDTO>))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(ResponseDTO<NewsDTO>))]
+        public IActionResult Create([FromBody] NewsDTO newsDTO)
+        {
+            var response = _newsService.CreateNews(newsDTO);
+            return StatusCode(response.StatusCode, response);
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(ResponseDTO<NewsDTO>))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(ResponseDTO<NewsDTO>))]
+        public IActionResult GetById(Guid id)
+        {
+            var response = _newsService.GetNewsById(id);
+            return StatusCode(response.StatusCode, response);
+
+        }
         
                                   
     }
