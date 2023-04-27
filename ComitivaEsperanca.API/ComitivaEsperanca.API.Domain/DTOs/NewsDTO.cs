4b3564ddceb9ae4e14b51e1d91bfdd6560cf12f4
Eduardo Fuso <eduardo.fuso@sesims.com.br>
2023-04-26 21:15:10 -0400

    Implementando DTO de Noticia

---
+++ ComitivaEsperanca.API/ComitivaEsperanca.API.Domain/DTOs/NewsDTO.cs
@@ -0,0 +1,21 @@
+﻿namespace ComitivaEsperanca.API.Domain.DTOs
+{
+    public class NewsDTO
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string NewsContent { get; set; }
+        public DateTime PublicationDate { get; set; }
+        public string CommodityType { get; set; }
+        public string Source { get; set; }
+        public double PositiveSentiment { get; set; }
+        public double NeutralSentiment { get; set; }
+        public double NegativeSentiment { get; set; }
+        public double FinalSentiment { get; set; }
+
+        public NewsDTO()
+        {
+            
+        }
+    }
+}
