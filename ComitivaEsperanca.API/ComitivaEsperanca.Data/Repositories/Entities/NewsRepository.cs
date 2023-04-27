a9ceb0842722963b9923f289d86df524b9c0125a
Eduardo Fuso <eduardo.fuso@sesims.com.br>
2023-04-26 21:16:54 -0400

    Implementando repositório de Noticia

---
+++ ComitivaEsperanca.API/ComitivaEsperanca.Data/Repositories/Entities/NewsRepository.cs
@@ -0,0 +1,14 @@
+﻿using ComitivaEsperanca.API.Data.Context;
+using ComitivaEsperanca.API.Domain.Entities;
+using ComitivaEsperanca.API.Domain.Interfaces.Repositories.Entities;
+
+namespace ComitivaEsperanca.API.Data.Repositories.Entities
+{
+    public class NewsRepository : BaseRepository<News>, INewsRepository
+    {
+        public NewsRepository(CoreContext context) : base(context)
+        {
+
+        }
+    }
+}
