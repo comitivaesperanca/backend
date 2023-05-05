bd98440a5df97f1ff9d5abf0cfa99b80c8188aa3
Eduardo Fuso <eduardo.fuso@sesims.com.br>
2023-05-04 20:14:40 -0400

    Adicionando endpoint de edição de noticias

--- ComitivaEsperanca.API/ComitivaEsperanca.API/Controllers/NewsController.cs
+++ ComitivaEsperanca.API/ComitivaEsperanca.API/Controllers/NewsController.cs
@@ -2,6 +2,7 @@
 using ComitivaEsperanca.API.Domain.Entities;
 using ComitivaEsperanca.API.Service.Services;
 using Microsoft.AspNetCore.Mvc;
+using System.Net.Mime;
 
 namespace ComitivaEsperanca.API.Controllers
 {
@@ -25,6 +26,15 @@ namespace ComitivaEsperanca.API.Controllers
             return StatusCode(response.StatusCode, response);
         }
 
+        [HttpPut]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(ResponseDTO<NewsDTO>))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(ResponseDTO<NewsDTO>))]
+        public IActionResult Update([FromBody] NewsDTO newsDTO)
+        {
+            var response = _newsService.UpdateNews(newsDTO);
+            return StatusCode(response.StatusCode, response);
+        }
+
         [HttpGet("{id}")]
         [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(ResponseDTO<NewsDTO>))]
         [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(ResponseDTO<NewsDTO>))]
@@ -42,7 +52,20 @@ namespace ComitivaEsperanca.API.Controllers
             var response = _newsService.GetTotalNews();
             return StatusCode(StatusCodes.Status200OK, response);
         }
-        
+
+        [HttpGet("Paginated")]
+        [Produces(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(ResponseDTO<List<NewsDTO>>))]
+        public async Task<PaginatedItemsDTO<NewsDTO>> GetPaginated(
+            [FromQuery] string? search,
+            [FromQuery] string? sentiment,
+            [FromQuery] DateTime? date,
+            [FromQuery] string? source,
+            [FromQuery] int pageSize = 10,
+            [FromQuery] int pageIndex = 1)
+        {
+            return await _newsService.GetListAsync(search, sentiment, date, source, pageSize, pageIndex);
+        }
                                   
     }
 }
