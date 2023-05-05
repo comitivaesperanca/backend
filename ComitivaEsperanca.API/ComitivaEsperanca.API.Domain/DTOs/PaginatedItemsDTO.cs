a77a406c2bcef6b81fddd8f7c6ea393edc803890
Eduardo Fuso <eduardo.fuso@sesims.com.br>
2023-05-04 20:15:19 -0400

    Implementando DTO de itens paginados

---
+++ ComitivaEsperanca.API/ComitivaEsperanca.API.Domain/DTOs/PaginatedItemsDTO.cs
@@ -0,0 +1,18 @@
+﻿namespace ComitivaEsperanca.API.Domain.DTOs
+{
+    public class PaginatedItemsDTO<T>
+    {
+        public int PageIndex { get; }
+        public int PageSize { get; }
+        public int TotalItems { get; }
+        public ICollection<T> ItemsOnPage { get; }
+
+        public PaginatedItemsDTO(int pageIndex, int pageSize, int totalItems, ICollection<T> itemsOnPage)
+        {
+            PageIndex = pageIndex;
+            PageSize = pageSize;
+            TotalItems = totalItems;
+            ItemsOnPage = itemsOnPage;
+        }
+    }
+}
